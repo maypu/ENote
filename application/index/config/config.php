513f4f57f61142f3119ce1f3dd50ea24afa01ce3
maypu <peng5649360@126.com>
2019-06-26 00:43:54 +0800

    test config
    test config

--- application/index/config/config.php
+++ application/index/config/config.php
@@ -18,11 +18,11 @@ return [
     'rsa_client_public_key' => './RSA/rsa_client_public_key.pem',
 
 	//GitHub OAuth2.0
-	'github_client_id' => 'f7c9784321adfa763b30',
-	'github_client_secret' => '81a3fd40c285db29b3aef7193e3c0f5d90e210e2',
+	'github_client_id' => 'f7c9784321adfa763',
+	'github_client_secret' => '81a3fd40c285db29b3aef7193e',
 
 	//Weixin Token
-	'weixin_appID' => 'wxe620e2a616827dd6',
-	'weixin_appsecret' => 'abdce9ab8a59b35cc8b01138548644d1',
-	'weixin_template_id' => '2C2xbxhm0o8PBLEcmVusCfHLD4xsvh2_gRCoSDN6WnA',
+	'weixin_appID' => 'wxe620e2a61682',
+	'weixin_appsecret' => 'abdce9ab8a59b35cc8b0113',
+	'weixin_template_id' => '2C2xbxhm0o8PBLEcmVusCfHLD4xsvh2_gRC',
 ];
