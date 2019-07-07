8256dc08d5006d4773281442b6c3279d6f5f7c6d
maypu <peng5649360@126.com>
2019-07-07 21:07:31 +0800

    supply commit

--- runtime/temp/8ea04774826f167ebc06a547500bacc5.php
+++ runtime/temp/8ea04774826f167ebc06a547500bacc5.php
@@ -1,4 +1,4 @@
-<?php /*a:1:{s:76:"E:\wamp\www\phpStrom_Project\EPusher\application\index\view\index\index.html";i:1562081215;}*/ ?>
+<?php /*a:1:{s:76:"E:\wamp\www\phpStrom_Project\EPusher\application\index\view\index\index.html";i:1562493237;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,60 +7,58 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel="stylesheet" href="static/dist/layui/css/layui.css">
-    <link rel="stylesheet" href="static/css/main.css">
-    <style>
-        .layui-nav-child{
-            z-index: 1001;
-        }
-    </style>
+    <link rel="stylesheet" href="static/css/index.css">
 </head>
-<body class="lay-blog">
-    <div></div>
-    <div class="header">
-        <div class="header-wrap">
-            <h1 class="logo pull-left">
-                <a href="index.html">
-                    <img src="static/images/logo.png" alt="" class="logo-img">
-                    <img src="static/images/logo-text.png" alt="" class="logo-text">
-                </a>
-            </h1>
-            <!--<form class="layui-form blog-seach pull-left" action="">-->
-                <!--<div class="layui-form-item blog-sewrap">-->
-                    <!--<div class="layui-input-block blog-sebox">-->
-                        <!--<i class="layui-icon layui-icon-search"></i>-->
-                        <!--<input type="text" name="title" lay-verify="title" autocomplete="off"  class="layui-input">-->
-                    <!--</div>-->
-                <!--</div>-->
-            <!--</form>-->
-            <div class="blog-nav pull-right">
-                <ul class="layui-nav pull-left">
-                    <li class="layui-nav-item layui-this"><a href="index.html">首页</a></li>
-                    <li class="layui-nav-item"><a href="message.html">文档</a></li>
-                    <li class="layui-nav-item"><a href="about.html">价格</a></li>
-                    <li class="layui-nav-item">
-                        <a href="javascript:;" class="personal pull-left" ><img src="//t.cn/RCzsdCq" class="layui-nav-img">maypu</a>
-                        <dl class="layui-nav-child">
-                            <dd><a href="javascript:;">个人信息</a></dd>
-                            <dd><a href="javascript:;">退出登录</a></dd>
-                        </dl>
-                    </li>
-                </ul>
-            </div>
-            <div class="mobile-nav pull-right" id="mobile-nav">
-                <a href="javascript:;">
-                    <i class="layui-icon layui-icon-more"></i>
-                </a>
+<body>
+    <!-- 顶部导航栏 -->
+    <div class="layui-header header">
+        <div class = "layui-row">
+            <div class = "layui-col-lg12">
+                <div class="left-menu">
+                    <a href="">
+                        <div id="logo">
+<!--                            <img src="static/images/logo.png" width="10%">-->
+                        </div>
+                    </a>
+                </div>
+                <div class = "layui-hide-xs menu">
+                    <ul class="layui-nav" lay-filter="">
+                        <span>EPusher</span>
+                        <li class="layui-nav-item"><a href="/home/multiple"><i class="layui-icon layui-icon-upload"></i> 多图上传</a></li>
+                        <li class="layui-nav-item"><a href="/found"><i class="layui-icon layui-icon-search"></i> 探索发现</a></li>
+                        <li class="layui-nav-item"><a href="/home/log"><i class="layui-icon layui-icon-notice"></i> 更新日志</a></li>
+                        <li class="layui-nav-item">
+                            <a href="#" rel = "nofollow"><i class="layui-icon">&#xe705;</i> 帮助文档</a>
+                            <dl class="layui-nav-child"> <!-- 二级菜单 -->
+                                <dd><a href="https://www.xiaoz.me/doc/doc-imgurl/install" rel = "nofollow" target = "_blank">安装ImgURL</a></dd>
+                                <dd><a href="https://www.xiaoz.me/doc/doc-imgurl/api" rel = "nofollow" target = "_blank">ImgURL API</a></dd>
+                            </dl>
+                        </li>
+                        <li class="layui-nav-item"><a href="https://github.com/helloxz/imgurl" target = "_blank" rel = "nofollow"><i class="layui-icon">&#xe635;</i> 源码</a></li>
+                        <li class="layui-nav-item"><a href="/page/use"><i class="layui-icon">&#xe60b;</i> 关于</a></li>
+                        <!-- 简单判断用户是否登录 -->
+                        <li class="layui-nav-item"><a href="/admin/index"><i class="layui-icon layui-icon-console"></i> 后台管理</a></li>
+                        <!-- 简单判断用户是否登录END -->
+                    </ul>
+                </div>
+                <div class = "menu layui-hide-lg layui-hide-md layui-hide-sm">
+                    <ul class="layui-nav" lay-filter="">
+                        <li class="layui-nav-item"><a href="/found"><i class="layui-icon layui-icon-search"></i> 发现</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-        <ul class="pop-nav" id="pop-nav">
-            <li><a href="index.html">首页</a></li>
-            <li><a href="message.html">文档</a></li>
-            <li><a href="about.html">价格</a></li>
-            <li><a href="about.html">登录</a></li>
-            <li><a href="about.html">个人信息</a></li>
-            <li><a href="about.html">注销</a></li>
-        </ul>
     </div>
+    <div>
+        <img class="banner" src="static/images/banner-3.png" alt="banner">
+    </div>
+    <!-- 顶部导航栏 End -->
+    <div class="layui-container">
+        <div class="layui-row">
+
+        </div>
+    </div>
+
     <script src="static/dist/layui-v2.4.5/layui.js" charset="utf-8"></script>
     <script>
         layui.config({
